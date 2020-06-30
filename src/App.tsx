acda2514ee3a632283e94d49eb119c35c3122c3b
ItaloRayonne <rayoneitalo@gmail.com>
2020-06-30 12:42:03 -0300

    initial commit v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import React, {useState, useCallback} from 'react';
-import {ThemeProvider} from 'styled-components';
-
 import Layout from './components/Layout';
 import GlobalStyles from './styles/GlobalStyles';
-import dark from './styles/themes/dark';
-import light from './styles/themes/light';
 
 function App() {
-  const [theme, setTheme] = useState(() => {
-    const existingTheme = localStorage.getItem('@application:theme');
-
-    if (existingTheme) {
-      return existingTheme === 'light' ? light : dark;
-    }
-
-    return light;
-  });
-
-  const toggleTheme = useCallback(() => {
-    setTheme(theme.name === 'light' ? dark : light);
-  }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <Layout toggleTheme={toggleTheme}/>
+    <>
+      <Layout />
       <GlobalStyles />
-    </ThemeProvider>
+    </>
   );
 }
 
